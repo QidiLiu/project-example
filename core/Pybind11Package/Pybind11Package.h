2fb348b568a511f58092e2f9ed2ae1cc870ed646
QidiLiu <qidi.liu@outlook.com>
2023-08-15 14:57:08 +0800

    tested spdlog pybind11

--- core/Pybind11Package/Pybind11Package.h
+++ core/Pybind11Package/Pybind11Package.h
@@ -1,8 +1,12 @@
 #ifndef PROJECTEXAMPLE_PYBIND11PACKAGE_H_
 #define PROJECTEXAMPLE_PYBIND11PACKAGE_H_
 
+#include <string>
+
 #include <pybind11/pybind11.h>
-#include "spdlog/spdlog.h"
+#include "spdlog/logger.h"
+#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/sinks/rotating_file_sink.h"
 
 namespace py = pybind11;
 
@@ -10,10 +14,18 @@ class Pybind11Package {
 
 public:
 
-    Pybind11Package();
+    bool dev_mode;
 
-    ~Pybind11Package();
+    Pybind11Package(const std::string& init_purpose);
 
+    void setPurpose(const std::string& in_purpose);
+
+    const std::string getPurpose();
+
+private:
+
+    std::string purpose;
+    std::shared_ptr<spdlog::logger> log_ptr;
 };
 
 #endif // PROJECTEXAMPLE_PYBIND11PACKAGE_H_
