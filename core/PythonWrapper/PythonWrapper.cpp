d2c5def5342c50b822f7f071e45fbc45da3ba6f2
QidiLiu <qidi.liu@outlook.com>
2023-08-16 08:49:09 +0800

    added function: copy .pyd file to "${ROOT_DIR}/out/pyd" after building process

--- core/PythonWrapper/PythonWrapper.cpp
+++ core/PythonWrapper/PythonWrapper.cpp
@@ -4,7 +4,7 @@
 PYBIND11_MODULE(core, m) {
     m.doc() = "Interface of modules for Python. Developed by QidiLiu.";
     
-    py::class_<ModuleName1>(m, "ModuleName1", py::dynamic_attr())
+    py::class_<ModuleName1>(m, "TestClass", py::dynamic_attr())
         .def(py::init<const std::string&>())
         .def_readwrite("dev_mode", &ModuleName1::dev_mode)
         .def_property("purpose", &ModuleName1::setPurpose, &ModuleName1::getPurpose)
